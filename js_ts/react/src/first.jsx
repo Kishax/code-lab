1e06080cfbdedf7e59d09f0e52af65dff048e37b
bella <maekawatakaya10.8@gmail.com>
2025-04-08 12:43:46 +0900

    run react component in html file by using parcel-bundler

---
+++ js_ts/react/src/first.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<MyApp />);

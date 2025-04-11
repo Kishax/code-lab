24ccc176b65339fe06625b81569eda3ade2f8e8d
bella <maekawatakaya10.8@gmail.com>
2025-04-12 07:12:47 +0900

    send easy embed

--- python/src/main.py
+++ python/src/main.py
@@ -1,6 +1,6 @@
 import discord
 
-from config.config import TOKEN
+from config.config import CHANNEL_ID, TOKEN
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -11,6 +11,13 @@ async def on_ready():
   print("login successfully")
   game = discord.Game("Python")
   await client.change_presence(status=discord.Status.online, activity=game)
+
+  embed = discord.Embed(description="test")
+  channel = client.get_channel(CHANNEL_ID)
+  if channel and isinstance(channel, discord.TextChannel):
+    await channel.send(embed=embed)
+  else:
+    print(f"Error: Channel with ID {CHANNEL_ID} not found or is not a text channel.")
 
 
 client.run(TOKEN)
